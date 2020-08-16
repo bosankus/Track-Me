0e2bd60e32977adc415264c40ed510a0d6595584
Ankush Bose <ankushbose5@gmail.com>
2020-08-16 11:40:48 +0530

    Final Touch: StatisticsFragment.kt is showing graph now Previous issues are yet to solve. Bug fixing left.

--- app/src/main/java/in/androidplay/trackme/ui/fragment/StatisticsFragment.kt
+++ app/src/main/java/in/androidplay/trackme/ui/fragment/StatisticsFragment.kt
@@ -3,11 +3,17 @@ package `in`.androidplay.trackme.ui.fragment
 import `in`.androidplay.trackme.R
 import `in`.androidplay.trackme.data.viewmodel.StatisticsViewModel
 import `in`.androidplay.trackme.util.TimeFormatUtil.getFormattedStopwatchTime
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_statistics.*
 import kotlin.math.round
@@ -19,10 +25,11 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setListeners()
+        setObservers()
+        setupBarChart()
     }
 
-    private fun setListeners() {
+    private fun setObservers() {
         statisticsViewModel.totalTimeRun.observe(viewLifecycleOwner, Observer {
             it?.let {
                 val totalTimeRun = "Total Time: " + getFormattedStopwatchTime(it)
@@ -48,5 +55,41 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
                 tvTotalCalories.text = totalCalories
             }
         })
+        statisticsViewModel.runSortedByDate.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val allAvgSpeed = it.indices.map { i -> BarEntry(i.toFloat(), it[i].avgSpeedInKMH) }
+                val barDataSet = BarDataSet(allAvgSpeed, "Speed/Time").apply {
+                    valueTextColor = Color.BLACK
+                    color = ContextCompat.getColor(requireContext(), R.color.grey)
+                }
+                barChart.data = BarData(barDataSet)
+                barChart.invalidate()
+            }
+        })
+    }
+
+
+    private fun setupBarChart() {
+        barChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            setDrawLabels(false)
+            setDrawGridLines(false)
+            axisLineColor = Color.BLACK
+            textColor = Color.BLACK
+        }
+        barChart.axisLeft.apply {
+            setDrawGridLines(false)
+            axisLineColor = Color.BLACK
+            textColor = Color.BLACK
+        }
+        barChart.axisRight.apply {
+            setDrawGridLines(false)
+            axisLineColor = Color.BLACK
+            textColor = Color.BLACK
+        }
+        barChart.apply {
+            description.text = "Speed/Time"
+            legend.isEnabled = false
+        }
     }
 }
