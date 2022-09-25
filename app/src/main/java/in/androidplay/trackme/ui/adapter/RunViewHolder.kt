e5d93a828938dc7cf835b9c5abed719804c0f4a6
bosankus <ankush@androidplay.in>
2022-09-25 13:06:14 +0530

    Removed synthetic import in RunViewHolder.kt - replaced with viewbinding

--- app/src/main/java/in/androidplay/trackme/ui/adapter/RunViewHolder.kt
+++ app/src/main/java/in/androidplay/trackme/ui/adapter/RunViewHolder.kt
@@ -1,12 +1,11 @@
 package `in`.androidplay.trackme.ui.adapter
 
 import `in`.androidplay.trackme.data.room.Run
+import `in`.androidplay.trackme.databinding.LayoutRunListItemBinding
 import `in`.androidplay.trackme.util.TimeFormatUtil.getFormattedStopwatchTime
 import android.annotation.SuppressLint
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.layout_run_list_item.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -15,19 +14,20 @@ import java.util.*
  * Author: Ankush
  * On: 8/9/2020, 10:32 PM
  */
-class RunViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class RunViewHolder(private val binding: LayoutRunListItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 
     @SuppressLint("SetTextI18n")
     fun bind(runItem: Run) {
-        itemView.tvDistanceTravelled.text = "Distance: ${runItem.distanceInMeters / 1000f} km"
-        itemView.tvAvgSpeed.text = "Avg. Speed: ${runItem.avgSpeedInKMH} km/H"
-        itemView.tvCaloriesBurned.text = "Calories: ${runItem.caloriesBurnt} kcal"
-        itemView.tvTimeTaken.text = "Duration: " + getFormattedStopwatchTime(runItem.timeInMillis)
-        itemView.tvDateTimeStamp.text = "Date: " +
-            SimpleDateFormat("dd.MM.yy", Locale.getDefault()).format(runItem.timestamp)
+        binding.tvDistanceTravelled.text = "Distance: ${runItem.distanceInMeters / 1000f} km"
+        binding.tvAvgSpeed.text = "Avg. Speed: ${runItem.avgSpeedInKMH} km/H"
+        binding.tvCaloriesBurned.text = "Calories: ${runItem.caloriesBurnt} kcal"
+        binding.tvTimeTaken.text = "Duration: " + getFormattedStopwatchTime(runItem.timeInMillis)
+        binding.tvDateTimeStamp.text = "Date: " +
+                SimpleDateFormat("dd.MM.yy", Locale.getDefault()).format(runItem.timestamp)
         Glide.with(itemView.context)
             .load(runItem.img)
             .centerCrop()
-            .into(itemView.imgMap)
+            .into(binding.imgMap)
     }
 }
